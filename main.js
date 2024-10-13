c0206181641f7416632efb12b19bce446c3d4510
farhan <mmhdfarhan08@gmail.com>
2024-10-13 20:11:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+// alert('Hay asssalamuaikum')
+const body = document.body
+
+const h1 = document.createElement('h1')
+// h1.textContent = '<marquee>ANJAY INI H1 BRO</marquee>'
+
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML = '<marquee>Terimakasih Sudah Mampir ke Website Kami😊🙌👌</marquee>'
+
+const namaKamu = document.createElement('b')
+// namaKamu.innerText = '<marquee>NAYla Taqqiya</marquee>'
+
+body.append(h1)
+body.append(namaSaya)
+body.append(namaKamu)
+// var usia = 20
+// console.log('Hey hoo usia Admin ' + usia) 
+
+// let usia = 20
+// usia = 18
+// console.log('heyhoo usia kamu adalah ' + usia)
+// alert('usia admin adalah ' + usia)
+prompt('Masukkan usia anda?')
+
+// let usia  = prompt('berapa usia kamu?')
+// alert('usia anda adalah ' + usia) 
+
+// let nama = 'farhan'
+// let usia = 18
+// let tinggiBadan = 168.5
+// let beratBadan
+// let pacar = 2432434
+
+// beratBadan = 60
+
+// let saldoAwal = 300000
+// let saldoTambahan = 500000
+// const saldoAkhir = saldoAwal + saldoTambahan
+
+// if(pacar == null){
+//     pacar = 'belum punya'
+// }else {
+//     pacar = 'udah punya'
+// }
+
+// switch(pacar) {
+//     case 1:
+//         pacar = 'punya 1 aja'
+//         break
+//     case 2:
+//         pacar = 'punya pacar 2, aku cukup playboy'
+//         break
+//     default:
+//         pacar = 'belum punya pacar'
+//         break
+
+// }
+
+// alert(`nama saya ${nama} usia saya itu ${usia} tinggi Badan saya adalah ${tinggiBadan}cm berat badan saya ${beratBadan}kg dan pacar saya ${pacar}`)
+
+// alert(
+//     `saldo awal saya sebesar RP.${saldoAwal} & saldo tambahan yang akan saya miliki sebesar RP.${saldoTambahan} jadi total saldo yang saya miliki adalah sebanyak Rp.${saldoAkhir}`,
+// )
+// alert('nama anda adalah ' + nama + ' dan usia anda adalah' + usia)
+
+
+
+// console.log(document.body)
